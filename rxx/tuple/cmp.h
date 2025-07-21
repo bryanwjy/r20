495bef45020c721db938faaf4384c492113c4996
Bryan Wong <wongjengyan@gmail.com>
2025-07-22 02:14:02 +0900

    missing owning_view and 3-way tuple_cmp fix

--- rxx/tuple/cmp.h
+++ rxx/tuple/cmp.h
@@ -10,14 +10,49 @@ RXX_DEFAULT_NAMESPACE_BEGIN
 namespace details {
 namespace tuple {
 
-template <tuple_like T, tuple_like U, size_t... Is>
+inline constexpr struct three_way_synthesizer_t {
+    template <typename L, typename R>
+    requires requires(L const& left, R const& right) {
+        left < right ? true : false;
+        right < left ? false : true;
+    }
+    RXX_ATTRIBUTES(_HIDE_FROM_ABI, NODISCARD) RXX_STATIC_CALL constexpr auto
+    operator()(L const& left, R const& right) RXX_CONST_CALL noexcept([]() {
+        if constexpr (std::three_way_comparable_with<L, R>) {
+            return noexcept(
+                std::declval<L const&>() <=> std::declval<R const&>());
+        } else {
+            return noexcept(std::declval<L const&>() <
+                std::declval<R const&>())&& noexcept(std::declval<R const&>() <
+                std::declval<L const&>());
+        }
+    }()) {
+        if constexpr (std::three_way_comparable_with<L, R>) {
+            return left <=> right;
+        } else {
+            if (left < right) {
+                return std::weak_ordering::less;
+            } else if (right < left) {
+                return std::weak_ordering::greater;
+            } else {
+                return std::weak_ordering::equivalent;
+            }
+        }
+    }
+} three_way_synthesizer = {};
+
+template <tuple_like L, tuple_like R, size_t... Is>
 __RXX_HIDE_FROM_ABI auto three_way_result(
-    T const& l, U const& r, std::index_sequence<Is...>) noexcept
-    -> std::common_comparison_category_t<decltype(ranges::get_element<Is>(l) <=>
-        ranges::get_element<Is>(r))...>;
+    L const&, R const&, std::index_sequence<Is...>) noexcept
+    -> std::common_comparison_category_t<
+        std::invoke_result_t<three_way_synthesizer_t, decl_element_t<Is, L>,
+            decl_element_t<Is, R>>...>;
 
 template <tuple_like L, tuple_like R>
-requires (std::tuple_size_v<L> == std::tuple_size_v<R>)
+requires (std::tuple_size_v<L> == std::tuple_size_v<R>) && requires {
+    three_way_result(std::declval<L>(), std::declval<R>(),
+        sequence_for<std::remove_cvref_t<L>>);
+}
 using three_way_result_t RXX_NODEBUG =
     decltype(three_way_result(std::declval<L>(), std::declval<R>(),
         sequence_for<std::remove_cvref_t<L>>));
@@ -27,8 +62,8 @@ requires (std::tuple_size_v<L> == std::tuple_size_v<R>)
 inline constexpr bool is_nothrow_three_way_v = requires {
     typename three_way_result_t<L, R>;
     requires ([]<size_t... Is>(std::index_sequence<Is...>) {
-        return (...&& noexcept(
-            decl_element<Is, L const&>() <=> decl_element<Is, R const&>()));
+        return (...&& noexcept(three_way_synthesizer(
+            decl_element<Is, L const&>(), decl_element<Is, R const&>())));
     }(sequence_for<std::remove_cvref_t<L>>));
 } && is_nothrow_accessible_v<L> && is_nothrow_accessible_v<R>;
 
@@ -69,17 +104,20 @@ inline constexpr bool is_nothrow_less_comparable_v = requires {
 } && is_nothrow_accessible_v<L> && is_nothrow_accessible_v<R>;
 
 template <size_t I, tuple_like L, tuple_like R>
-requires (I == std::tuple_size_v<L>)
+requires (I == std::tuple_size_v<L>) &&
+    requires { typename three_way_result_t<L, R>; }
 __RXX_HIDE_FROM_ABI constexpr three_way_result_t<L, R> three_way(
     L const&, R const&) noexcept {
-    return three_way_result_t<L, R>::equal;
+    return three_way_result_t<L, R>::equivalent;
 }
 
 template <size_t I, tuple_like L, tuple_like R>
-requires (I < std::tuple_size_v<L>)
+requires (I < std::tuple_size_v<L>) &&
+    requires { typename three_way_result_t<L, R>; }
 __RXX_HIDE_FROM_ABI constexpr three_way_result_t<L, R> three_way(
     L const& l, R const& r) {
-    auto const result = ranges::get_element<I>(l) <=> ranges::get_element<I>(r);
+    auto const result = three_way_synthesizer(
+        ranges::get_element<I>(l), ranges::get_element<I>(r));
     return result != 0 ? result : three_way<I + 1>(l, r);
 }
 
@@ -145,31 +183,40 @@ RXX_ATTRIBUTES(_HIDE_FROM_ABI, FLATTEN) constexpr bool less(
 } // namespace details
 
 template <typename... TTypes, typename... UTypes>
-RXX_ATTRIBUTES(_HIDE_FROM_ABI, NODISCARD)
-constexpr bool
+requires requires(tuple<TTypes...> const& lhs, tuple<UTypes...> const& rhs) {
+    details::tuple::equals(lhs, rhs);
+}
+RXX_ATTRIBUTES(_HIDE_FROM_ABI, NODISCARD) constexpr bool
 operator==(tuple<TTypes...> const& lhs, tuple<UTypes...> const& rhs) noexcept(
     noexcept(details::tuple::equals(lhs, rhs))) {
     return details::tuple::equals(lhs, rhs);
 }
 
 template <typename... TTypes, typename... UTypes>
-RXX_ATTRIBUTES(_HIDE_FROM_ABI, NODISCARD)
-constexpr bool
+requires requires(tuple<TTypes...> const& lhs, tuple<UTypes...> const& rhs) {
+    details::tuple::less(lhs, rhs);
+}
+RXX_ATTRIBUTES(_HIDE_FROM_ABI, NODISCARD) constexpr bool
 operator<(tuple<TTypes...> const& lhs, tuple<UTypes...> const& rhs) noexcept(
     noexcept(details::tuple::less(lhs, rhs))) {
     return details::tuple::less(lhs, rhs);
 }
 
 template <typename... TTypes, typename... UTypes>
-RXX_ATTRIBUTES(_HIDE_FROM_ABI, NODISCARD)
-constexpr auto
+requires requires(tuple<TTypes...> const& lhs, tuple<UTypes...> const& rhs) {
+    details::tuple::three_way(lhs, rhs);
+}
+RXX_ATTRIBUTES(_HIDE_FROM_ABI, NODISCARD) constexpr auto
 operator<=>(tuple<TTypes...> const& lhs, tuple<UTypes...> const& rhs) noexcept(
     noexcept(details::tuple::three_way(lhs, rhs))) {
     return details::tuple::three_way(lhs, rhs);
 }
 
 template <typename... TTypes, tuple_like Tuple>
-requires (sizeof...(TTypes) == std::tuple_size_v<Tuple>)
+requires (sizeof...(TTypes) == std::tuple_size_v<Tuple>) &&
+    requires(tuple<TTypes...> const& lhs, Tuple const& rhs) {
+        details::tuple::equals(lhs, rhs);
+    }
 RXX_ATTRIBUTES(_HIDE_FROM_ABI, NODISCARD) constexpr bool operator==(
     tuple<TTypes...> const& lhs,
     Tuple const& rhs) noexcept(noexcept(details::tuple::equals(lhs, rhs))) {
@@ -177,7 +224,10 @@ RXX_ATTRIBUTES(_HIDE_FROM_ABI, NODISCARD) constexpr bool operator==(
 }
 
 template <typename... TTypes, tuple_like Tuple>
-requires (sizeof...(TTypes) == std::tuple_size_v<Tuple>)
+requires (sizeof...(TTypes) == std::tuple_size_v<Tuple>) &&
+    requires(tuple<TTypes...> const& lhs, Tuple const& rhs) {
+        details::tuple::three_way(lhs, rhs);
+    }
 RXX_ATTRIBUTES(_HIDE_FROM_ABI, NODISCARD) constexpr auto operator<=>(
     tuple<TTypes...> const& lhs,
     Tuple const& rhs) noexcept(noexcept(details::tuple::three_way(lhs, rhs))) {
--- rxx/tuple/fwd.h
+++ rxx/tuple/fwd.h
@@ -14,18 +14,23 @@ template <typename...>
 class tuple;
 RXX_DEFAULT_NAMESPACE_END
 
-template <typename... Ts, std::common_with<Ts>... Us>
-struct std::common_type<__RXX tuple<Ts...>, __RXX tuple<Us...>> {
-    using type RXX_NODEBUG = __RXX tuple<std::common_type_t<Ts, Us>...>;
+template <typename... TTypes, typename... UTypes>
+requires (sizeof...(TTypes) == sizeof...(UTypes)) &&
+    requires { typename __RXX tuple<std::common_type_t<TTypes, UTypes>...>; }
+struct std::common_type<__RXX tuple<TTypes...>, __RXX tuple<UTypes...>> {
+    using type = typename __RXX tuple<std::common_type_t<TTypes, UTypes>...>;
 };
 
-template <typename... Ts, typename... Us, template <typename> class TQual,
-    template <typename> class UQual>
-requires (... && std::common_reference_with<TQual<Ts>, UQual<Us>>)
-struct std::basic_common_reference<__RXX tuple<Ts...>, __RXX tuple<Us...>,
+template <typename... TTypes, typename... UTypes,
+    template <typename> class TQual, template <typename> class UQual>
+requires (sizeof...(TTypes) == sizeof...(UTypes)) && requires {
+    typename __RXX tuple<
+        __RXX common_reference_t<TQual<TTypes>..., UQual<UTypes>>...>;
+}
+struct std::basic_common_reference<std::tuple<TTypes...>, std::tuple<UTypes...>,
     TQual, UQual> {
     using type RXX_NODEBUG =
-        __RXX tuple<__RXX common_reference_t<TQual<Ts>, UQual<Us>>...>;
+        __RXX tuple< __RXX common_reference_t<TQual<TTypes>, UQual<UTypes>>...>;
 };
 
 template <size_t I, typename... Ts>
