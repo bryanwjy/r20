8121d250751f6f57a2146881941dc74608fe646a
Bryan Wong <wongjengyan@gmail.com>
2025-07-26 17:46:51 +0900

    simplify jump_table

--- rxx/details/jump_table.h
+++ rxx/details/jump_table.h
@@ -4,6 +4,7 @@
 
 #include "rxx/config.h"
 
+#include "rxx/type_traits/common_reference.h"
 #include "rxx/utility.h"
 
 #include <concepts>
@@ -28,12 +29,12 @@ public:
 };
 
 template <typename... Ts>
-requires (... || std::is_void_v<Ts>)
+requires (... && std::is_void_v<Ts>)
 __RXX_HIDE_FROM_ABI void make_multi_return() noexcept;
 
 template <typename... Ts>
 __RXX_HIDE_FROM_ABI auto make_multi_return() noexcept
-    -> std::common_reference_t<Ts...>;
+    -> common_reference_t<Ts...>;
 
 template <typename... Ts>
 using multi_return_t RXX_NODEBUG = decltype(make_multi_return<Ts...>());
@@ -42,6 +43,7 @@ template <typename T, T... Ns>
 class jump_table {
     static_assert(
         std::semiregular<T> && requires(T value) {
+            typename std::integral_constant<T, (..., Ns)>;
             [](T value, std::integral_constant<T, (..., Ns)> first) {
                 switch (value) {
                 case (first.value):
@@ -50,6 +52,7 @@ class jump_table {
                     return 1;
                 }
             }(value, {});
+            { (T[]){Ns...} };
         }, "Invalid jump table index type");
 
     __RXX_HIDE_FROM_ABI static constexpr size_t size = sizeof...(Ns);
@@ -59,117 +62,74 @@ class jump_table {
         return values[idx];
     }
 
-    using first_type RXX_NODEBUG = std::integral_constant<T, index_to_value(0)>;
     template <size_t I>
-    using ith_type RXX_NODEBUG = std::integral_constant<T, index_to_value(I)>;
+    struct ith_type : std::integral_constant<T, index_to_value(I)> {};
 
     template <typename F, typename... Args>
     requires std::invocable<F, T, Args...>
-    __RXX_HIDE_FROM_ABI static auto result_type_impl() noexcept
+    __RXX_HIDE_FROM_ABI static auto make_result_type() noexcept
         -> multi_return_t<std::invoke_result_t<F, T, Args...>,
             std::invoke_result_t<F, std::integral_constant<T, Ns>, Args...>...>;
     template <typename F, typename... Args>
-    __RXX_HIDE_FROM_ABI static auto result_type_impl() noexcept
+    __RXX_HIDE_FROM_ABI static auto make_result_type() noexcept
         -> multi_return_t<
             std::invoke_result_t<F, std::integral_constant<T, Ns>, Args...>...>;
 
 public:
     template <typename F, typename... Args>
-    using result_type RXX_NODEBUG = decltype(result_type_impl<F, Args...>());
+    using result_type RXX_NODEBUG = decltype(make_result_type<F, Args...>());
 
 private:
     template <typename F, typename... Args>
-    requires std::invocable<F, T, Args...>
-    __RXX_HIDE_FROM_ABI static constexpr auto default_(F&& callable, T value,
-        Args&&... args) noexcept(std::is_nothrow_invocable_v<F, T, Args...>)
-        -> decltype(auto) {
-        return std::invoke(
-            std::forward<F>(callable), value, std::forward<Args>(args)...);
-    }
-
-    template <typename F, typename... Args>
-    requires (!std::invocable<F, T, Args...>)
     __RXX_HIDE_FROM_ABI static constexpr result_type<F, Args...> default_(
-        F&& callable, T, Args&&...) noexcept {
-        static_assert(sizeof...(Args) == 0);
-        if constexpr (!std::is_void_v<result_type<F, Args...>>) {
+        F&& callable, T value, Args&&... args) {
+        if constexpr (std::invocable<F, T, Args...>) {
+            return std::invoke(
+                std::forward<F>(callable), value, std::forward<Args>(args)...);
+        } else if constexpr (!std::is_void_v<result_type<F, Args...>>) {
             RXX_BUILTIN_unreachable();
         }
         // If the callable returns nothing, the default case can be ignored
     }
 
     template <size_t I, typename F, typename... Args>
-    requires (I < size)
-    __RXX_HIDE_FROM_ABI static constexpr auto
-    case_(F&& callable, T, Args&&... args) noexcept(
-        std::is_nothrow_invocable_v<F, ith_type<I>, Args...>)
-        -> decltype(auto) {
-        return std::invoke(std::forward<F>(callable), ith_type<I>{},
-            std::forward<Args>(args)...);
+    __RXX_HIDE_FROM_ABI static constexpr auto case_(
+        F&& callable, T value, Args&&... args) -> decltype(auto) {
+        if constexpr (I < size) {
+            constexpr ith_type<I> case_arg{};
+            return std::invoke(std::forward<F>(callable), case_arg,
+                std::forward<Args>(args)...);
+        } else {
+            return default_(
+                std::forward<F>(callable), value, std::forward<Args>(args)...);
+        }
     }
 
     template <size_t I, typename F, typename... Args>
-    requires (I >= size)
-    __RXX_HIDE_FROM_ABI static constexpr auto case_(F&& callable, T value,
-        Args&&... args) noexcept(noexcept(default_(std::declval<F>(),
-        std::declval<T>(), std::declval<Args>()...))) -> decltype(auto) {
-        return default_(
-            std::forward<F>(callable), value, std::forward<Args>(args)...);
-    }
-
-    template <size_t O, typename F, typename... Args>
-    requires (O >= size)
-    __RXX_HIDE_FROM_ABI static constexpr auto next_(F&& callable, T value,
-        Args&&... args) noexcept(noexcept(default_(std::declval<F>(),
-        std::declval<T>(), std::declval<Args>()...))) -> decltype(auto) {
-        return default_(
-            std::forward<F>(callable), value, std::forward<Args>(args)...);
-    }
-
-    template <size_t O, typename F, typename... Args>
-    requires (O < size)
-    __RXX_HIDE_FROM_ABI static constexpr auto next_(F&& callable, T value,
-        Args&&... args) noexcept(noexcept(impl<O>(std::declval<F>(),
-        std::declval<T>(), std::declval<Args>()...))) -> decltype(auto) {
-        return impl<O>(
-            std::forward<F>(callable), value, std::forward<Args>(args)...);
-    }
-
-    __RXX_HIDE_FROM_ABI static consteval size_t page_size(size_t idx) noexcept {
-        if (size >= 16 + idx) {
-            return 16;
+    __RXX_HIDE_FROM_ABI static constexpr auto next_(
+        F&& callable, T value, Args&&... args) -> decltype(auto) {
+        if constexpr (I < size) {
+            return impl<I>(
+                std::forward<F>(callable), value, std::forward<Args>(args)...);
+        } else {
+            return default_(
+                std::forward<F>(callable), value, std::forward<Args>(args)...);
         }
-
-        if (size >= 8 + idx) {
-            return 8;
-        }
-
-        return size - idx;
     }
 
-    template <size_t O, typename F, typename... Args>
-    __RXX_HIDE_FROM_ABI static constexpr bool is_nothrow_dispatchable_v =
-        []<size_t... Is>(std::index_sequence<Is...>) {
-            return (noexcept(next_<O + page_size(O)>(std::declval<F>(),
-                        std::declval<T>(), std::declval<Args>()...)) &&
-                ...&& noexcept(case_<O + Is>(std::declval<F>(),
-                    std::declval<T>(), std::declval<Args>()...)));
-        }(std::make_index_sequence<page_size(O)>{});
-
-    template <size_t O, typename F, typename... Args>
+    template <size_t I, typename F, typename... Args>
     RXX_ATTRIBUTES(_HIDE_FROM_ABI, FLATTEN)
-    static constexpr auto impl(F&& callable, T value, Args&&... args) noexcept(
-        is_nothrow_dispatchable_v<O, F, Args...>) -> decltype(auto) {
+    static constexpr auto impl(F&& callable, T value, Args&&... args)
+        -> decltype(auto) {
 #define __RXX_JT_CASE(X)        \
-    case index_to_value(O + X): \
-        return case_<O + X>(    \
+    case index_to_value(I + X): \
+        return case_<I + X>(    \
             std::forward<F>(callable), value, std::forward<Args>(args)...)
 #define __RXX_JT_DEFAULT(X)  \
     default:                 \
-        return next_<O + X>( \
+        return next_<I + X>( \
             std::forward<F>(callable), value, std::forward<Args>(args)...)
-
-        if constexpr (size >= 16 + O) {
+        if constexpr (size >= 16 + I) {
             switch (value) {
                 __RXX_JT_CASE(0);
                 __RXX_JT_CASE(1);
@@ -189,7 +149,7 @@ private:
                 __RXX_JT_CASE(15);
                 __RXX_JT_DEFAULT(16);
             }
-        } else if constexpr (size >= 8 + O) {
+        } else if constexpr (size >= 8 + I) {
             switch (value) {
                 __RXX_JT_CASE(0);
                 __RXX_JT_CASE(1);
@@ -201,7 +161,7 @@ private:
                 __RXX_JT_CASE(7);
                 __RXX_JT_DEFAULT(8);
             }
-        } else if constexpr (size == 7 + O) {
+        } else if constexpr (size == 7 + I) {
             switch (value) {
                 __RXX_JT_CASE(0);
                 __RXX_JT_CASE(1);
@@ -212,7 +172,7 @@ private:
                 __RXX_JT_CASE(6);
                 __RXX_JT_DEFAULT(7);
             }
-        } else if constexpr (size == 6 + O) {
+        } else if constexpr (size == 6 + I) {
             switch (value) {
                 __RXX_JT_CASE(0);
                 __RXX_JT_CASE(1);
@@ -222,7 +182,7 @@ private:
                 __RXX_JT_CASE(5);
                 __RXX_JT_DEFAULT(6);
             }
-        } else if constexpr (size == 5 + O) {
+        } else if constexpr (size == 5 + I) {
             switch (value) {
                 __RXX_JT_CASE(0);
                 __RXX_JT_CASE(1);
@@ -231,7 +191,7 @@ private:
                 __RXX_JT_CASE(4);
                 __RXX_JT_DEFAULT(5);
             }
-        } else if constexpr (size == 4 + O) {
+        } else if constexpr (size == 4 + I) {
             switch (value) {
                 __RXX_JT_CASE(0);
                 __RXX_JT_CASE(1);
@@ -239,20 +199,20 @@ private:
                 __RXX_JT_CASE(3);
                 __RXX_JT_DEFAULT(4);
             }
-        } else if constexpr (size == 3 + O) {
+        } else if constexpr (size == 3 + I) {
             switch (value) {
                 __RXX_JT_CASE(0);
                 __RXX_JT_CASE(1);
                 __RXX_JT_CASE(2);
                 __RXX_JT_DEFAULT(3);
             }
-        } else if constexpr (size == 2 + O) {
+        } else if constexpr (size == 2 + I) {
             switch (value) {
                 __RXX_JT_CASE(0);
                 __RXX_JT_CASE(1);
                 __RXX_JT_DEFAULT(2);
             }
-        } else if constexpr (size == 1 + O) {
+        } else if constexpr (size == 1 + I) {
             switch (value) {
                 __RXX_JT_CASE(0);
                 __RXX_JT_DEFAULT(1);
@@ -275,10 +235,8 @@ public:
     // if return type void, default_case is optional
     template <typename F, std::convertible_to<T> U, typename... Args>
     requires requires { typename result_type<F, Args...>; }
-    __RXX_HIDE_FROM_ABI constexpr auto operator()(F&& callable, U&& value,
-        Args&&... args) const noexcept(noexcept(impl<0>(std::declval<F>(),
-        static_cast<T>(std::declval<U>()), std::declval<Args>()...)))
-        -> decltype(auto) {
+    __RXX_HIDE_FROM_ABI RXX_STATIC_CALL constexpr auto operator()(F&& callable,
+        U&& value, Args&&... args) RXX_CONST_CALL->decltype(auto) {
         return impl<0>(std::forward<F>(callable), static_cast<T>(value),
             std::forward<Args>(args)...);
     }
