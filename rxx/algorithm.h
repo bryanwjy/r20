7812048c187b11838e8449eb929bd0083a459842
Bryan Wong <wongjengyan@gmail.com>
2025-08-11 16:03:41 +0900

    missing include

--- rxx/algorithm.h
+++ rxx/algorithm.h
@@ -10,6 +10,7 @@
 #include "rxx/algorithm/copy.h"
 #include "rxx/algorithm/count.h"
 #include "rxx/algorithm/ends_with.h"
+#include "rxx/algorithm/equal.h"
 #include "rxx/algorithm/fill.h"
 #include "rxx/algorithm/find.h"
 #include "rxx/algorithm/find_end.h"
